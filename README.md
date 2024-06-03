460459a5daee705319e57882b09215c3579676cb
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-06-03 01:55:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Shouts
+
+Shouts is a utility module for creating and managing dynamic variables (shouts) in a global context. These shouts can be accessed and modified globally, making it easier to manage state and data across different parts of your application.
+
+## Features
+
+- **Dynamic Variables**: Create dynamic variables without knowing their return type.
+- **Global Access**: Access and modify shouts globally.
+- **Callbacks**: Define callbacks to be executed when shouts are accessed.
+- **Management**: Easily create, destroy, and check the existence of shouts.
---
+++ dist/shouts.mjs
@@ -0,0 +1,122 @@
+// src/shouts.ts
+var ShoutModule = () => ({
+  /** Core -> this
+   * @example ```ts
+   * ShoutModule.createShout('timergen', () => {...})
+   * ShoutModule.createShout('startTimer', () => {...})
+   *
+   * let timer = timergen;
+   *
+   * startTimer;
+   * console.log(timer)
+   *
+   * setInteral(() => {
+   *   console.log(timer) // X.X ms
+   * }, 1000)
+   * ```
+   */
+  ShoutModule,
+  /**
+   * A map to store shout data.
+   */
+  total: /* @__PURE__ */ new Map(),
+  /**
+   * Creates a new shout with the given name and callback.
+   * @param name The name of the shout.
+   * @param cb The callback to be executed when the shout is accessed.
+   */
+  createShout(name = "", cb = () => {
+  }, implicitReturnValue = null) {
+    let count = 0;
+    let keystore = 0;
+    this.total.set(name, {
+      count,
+      keystore,
+      cb,
+      implicitReturnValue
+    });
+    return Object.defineProperty(globalThis, name, {
+      get: () => {
+        count++;
+        let shoutData = this.total.has(name) ? this.total.get(name) : null;
+        if (shoutData) {
+          shoutData.count = count;
+          this.total.set(name, shoutData);
+        }
+        return !implicitReturnValue ? cb(count, shoutData) : Reflect.get(cb(count, shoutData), implicitReturnValue);
+      },
+      set: (value) => {
+        let keyName = typeof value === "string" ? value : value.name || `${keystore}_${typeof value}`;
+        let shoutData = this.total.get(name) || {};
+        shoutData[keyName] = value;
+        this.total.set(name, shoutData);
+        keystore++;
+      },
+      enumerable: true,
+      configurable: true
+    });
+  },
+  /**
+   * Destroys the shout with the given name.
+   * @param name The name of the shout to destroy.
+   * @throws Error if the shout does not exist.
+   */
+  destroyShout(name) {
+    if (!this.total.has(name))
+      throw new Error(`Shout ${name} not found`);
+    Reflect.deleteProperty(globalThis, name);
+    this.total.delete(name);
+  },
+  /**
+   * Destroys all shouts.
+   */
+  removeAllShouts() {
+    this.total.forEach((_, name) => {
+      Reflect.deleteProperty(globalThis, name);
+      this.total.delete(name);
+    });
+  },
+  /**
+   * Checks if a shout with the given name exists.
+   * @param name The name of the shout to check.
+   * @returns {boolean} True if the shout exists, false otherwise.
+   */
+  isShout(name) {
+    let shoutData = Reflect.get(globalThis, name);
+    return !this.total.has(name) || !shoutData || !shoutData[Symbol.for("shout")]?.is;
+  },
+  /**
+   * Retrieves the shout data for the given name.
+   * @param name The name of the shout.
+   * @returns The shout data or undefined if not found.
+   */
+  getShout(name) {
+    if (!this.isShout(name))
+      return;
+    return this.total.get(name);
+  },
+  /**
+   * Retrieves the type of the shout for the given name.
+   * @param name The name of the shout.
+   * @returns The type of the shout or undefined if not found.
+   */
+  getShoutType(name) {
+    return this.getShout(name)?.type;
+  },
+  /**
+   * Retrieves the count of the shout for the given name.
+   * @param name The name of the shout.
+   * @returns The count of the shout or undefined if not found.
+   */
+  getShoutCount(name) {
+    return this.getShout(name)?.count;
+  }
+});
+var Shouts = ShoutModule();
+Shouts.createShout("$_", () => globalThis);
+if (typeof window !== "undefined")
+  globalThis.Shouts = Shouts;
+export {
+  Shouts
+};
+//# sourceMappingURL=shouts.mjs.map
